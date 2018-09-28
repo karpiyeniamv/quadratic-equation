f40eb866fc4a2c8360271e27416c5003ad4388b0
Maryia Karpiyenia <karpiyenia.mv@gmail.com>
2018-09-28 14:55:26 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    equation = equation.replace(/ /g,'');
+    var regexp =  /x/ig;
+    var pos = regexp.exec(equation);
+    var a = parseInt(equation.slice(0,pos.index),10) ;
+    regexp.lastIndex+=2;
+    var fos = regexp.exec(equation);
+    var b = parseInt(equation.slice(pos.index+3,fos.index-1),10) ;
+    var c =  parseInt(equation.slice(fos.index+1,equation.length),10) ;
+    var d = parseInt(b*b - (4*a*c));
+    console.log (d);
+    var res1 = Math.round(( (-1)*b + Math.sqrt(d))/(2*a));
+    var res2 = Math.round (((-1)*b - Math.sqrt(d))/(2*a));
+    if (res1< res2)
+        return [res1,res2];
+    else
+        return [res2,res1];
+};
